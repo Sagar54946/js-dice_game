c8b24bb4a78bf75abb511f41981492cc73760af8
Sagar54946 <155992642+Sagar54946@users.noreply.github.com>
2024-01-29 20:31:20 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1);
+randomNumber1 = randomNumber1 + 1;
+
+var randomDiceImage = "dice"+randomNumber1+".png";
+var randomImageSource = "images/"+randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomImageSource2 ="images/dice"+randomNumber2+".png"
+
+document.querySelector(".img2").setAttribute("src",randomImageSource2);
+
+if(randomNumber1 > randomNumber2){
+document.querySelector("h1").innerHTML = "🏆 Player 1 Wins";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins 🏆";
+}
+else if(randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw!";
+}
+
